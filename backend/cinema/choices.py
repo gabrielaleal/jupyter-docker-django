581bd92676d8191bd5527df2655055679b7b7287
Gabriela Leal <gabriela@vinta.com.br>
2022-07-29 13:30:05 -0300

    Add app 'cinema' and models

---
+++ backend/cinema/choices.py
@@ -0,0 +1,14 @@
+from model_utils.choices import Choices
+
+
+MOVIE_GENRES = Choices(
+    ("action", "Action"),
+    ("comedy", "Comedy"),
+    ("drama", "Drama"),
+    ("fantasy", "Fantasy"),
+    ("horror", "Horror"),
+    ("mistery", "Mistery"),
+    ("romance", "Romance"),
+    ("thriller", "Thriller"),
+    ("science_fiction", "Science-Fiction"),
+)
--- backend/psql_jupyter/settings/base.py
+++ backend/psql_jupyter/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "common",
     "users",
+    "cinema",
     "django_extensions",
 ]
 
