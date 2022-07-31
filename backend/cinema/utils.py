16dd4da2b44d7c03056ae1727664c4289e40cf1a
Gabriela Leal <gabriela@vinta.com.br>
2022-07-30 23:51:54 -0300

    Add helpers

---
+++ backend/cinema/utils.py
@@ -0,0 +1,12 @@
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import PostgresLexer
+from sqlparse import format
+from django.db.models import QuerySet
+
+
+def print_sql(queryset: QuerySet):
+    formatted = format(str(queryset.query), reindent=True)
+    print(highlight(formatted, PostgresLexer(), TerminalFormatter()))
+
+    # https://roman.pt/posts/pretty-print-django-sql/
