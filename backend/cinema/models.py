f456bc16bf6d3dd1523bfcd413a3c56168ccf9a6
Gabriela Leal <gabriela@vinta.com.br>
2022-08-16 21:18:56 -0300

    Simplify models

--- backend/cinema/models.py
+++ backend/cinema/models.py
@@ -17,13 +17,12 @@ class Movie(models.Model):
     synopsis = models.TextField(blank=True)
     genre = models.CharField(choices=MOVIE_GENRES, max_length=255)
     release_date = models.DateField(null=True)
-    is_available_on_netflix = models.BooleanField(default=False)
-    imdb_rate = models.FloatField(null=True)
     cast = models.ManyToManyField(Person, related_name="movies_starred")
     director = models.ForeignKey(
         Person, related_name="movies_directed", null=True, on_delete=models.SET_NULL
     )
-    budget = models.FloatField(null=True)
+    is_available_on_netflix = models.BooleanField(default=False)
+    imdb_rate = models.FloatField(null=True)
 
     def __str__(self):
         return f"{self.title}"
@@ -31,8 +30,6 @@ class Movie(models.Model):
 
 class Award(models.Model):
     name = models.CharField(max_length=255)
-    popular_name = models.CharField(max_length=255, blank=True)
-    first_awarded_year = models.IntegerField(null=True)
 
     def __str__(self):
         return f"{self.name} ({self.popular_name})"
@@ -43,21 +40,8 @@ class Nomination(models.Model):
     category = models.CharField(max_length=255)
     year = models.IntegerField(null=True)
     is_winner = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = True
-
-
-class MovieNomination(Nomination):
     movie = models.ForeignKey(Movie, related_name="nominations", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.movie.title} - {self.category} ({self.year}) - {self.award.popular_name}"
-
-
-class PersonNomination(Nomination):
     person = models.ForeignKey(Person, related_name="nominations", on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, related_name="people_nominations", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.person.name} - {self.category} ({self.year}) - {self.award.popular_name}"
+        return f"{self.movie.title} - {self.category} ({self.year}) - {self.award.name}"
