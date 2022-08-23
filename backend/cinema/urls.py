f227566722797ee0cf6763084bea323520034f82
Gabriela Leal <gabriela@vinta.com.br>
2022-08-23 09:22:19 -0300

    Add views to application

--- backend/cinema/urls.py
+++ backend/cinema/urls.py
@@ -1,10 +1,12 @@
 from django.urls import re_path
 
-from cinema.views import movies_list
+from cinema.views import ArtistsListView, AwardsListView, MoviesListView
 
 
 app_name = "music"
 
 urlpatterns = [
-    re_path(r"^movies/list/", movies_list, name="movies"),
+    re_path(r"^movies/list/", MoviesListView.as_view(), name="movies"),
+    re_path(r"^artists/list/", ArtistsListView.as_view(), name="artists"),
+    re_path(r"^awards/list/", AwardsListView.as_view(), name="awards"),
 ]
--- backend/cinema/views.py
+++ backend/cinema/views.py
@@ -1,12 +1,48 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Count, Q, Subquery, OuterRef
 
-from cinema.models import Movie
+from cinema.models import Award, Movie, Person
 
 
-def movies_list(request):
+# def movies_list(request):
+#     template_name = "movies/movies_list.html"
+#     context = {
+#         "movies": Movie.objects.select_related("director").prefetch_related("cast").all(),
+#     }
+
+#     return render(request, template_name, context)
+
+
+class MoviesListView(ListView):
+    model = Movie
+    queryset = Movie.objects.select_related("director").prefetch_related("cast").all()
     template_name = "movies/movies_list.html"
-    context = {
-        "movies": Movie.objects.select_related("director").prefetch_related("cast").all(),
-    }
 
-    return render(request, template_name, context)
+
+class ArtistsListView(ListView):
+    model = Person
+    paginate_by = 5
+    queryset = Person.objects.annotate(
+        total_awards_won=Count("nominations", filter=Q(nominations__is_winner=True))
+    ).order_by("-total_awards_won")
+    template_name = "artists/artists_list.html"
+
+
+class AwardsListView(ListView):
+    model = Award
+    queryset = Award.objects.annotate(
+        most_nominated_movie_title=Subquery(
+            Movie.objects.annotate(
+                nominations_count=Count(
+                    "nominations", filter=Q(nominations__award_id=OuterRef("id"))
+                )
+            )
+            .order_by("-nominations_count")
+            .values("title")[:1]
+        )
+    )
+
+    template_name = "awards/awards_list.html"
+
+
+<QuerySet [<Movie: Inglourious Basterds>, <Movie: Once Upon a Time in... Hollywood>]>
